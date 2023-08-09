d830355c6a6b637cc44be5a91e90fdd6234c3c9f
Ayaan Jung <mymayaan@gmail.com>
2023-08-09 23:12:23 +0900

    first commit

---
+++ pofile/profile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "[Developer Profile]" << endl;
+    cout << "• Basic Information" << endl;
+    cout << " - Name : Ayaan.Jung" << endl;
+    cout << " - Adress : 22-1, Sangmyung univ road, Republic of Korea" << endl;
+    cout << " - Phone Num : Private" << endl;
+    cout << " - Github : https://github.com/J-Ayaan" << endl << endl;
+
+    cout << "• Education" << endl;
+    cout << " - Computer Software, Sangmyung University(Korea)" << endl << endl;
+
+    cout << "• Work Experience" << endl;
+    cout << " - Not yet" << endl << endl;
+
+    cout << "• Skills" << endl;
+    cout << " - Programing Language : C, C++, Java, Python, SQL, R" << endl << endl;
+
+    cout << "• Language" << endl;
+    cout << " - Korean : Native" << endl;
+    cout << " - German : Business Level - A1" << endl << endl;
+
+    cout << "• Extracurricular Activities" << endl;
+    cout << " - Military : 7th Infantry Division" << endl << endl;
+
+    cout << "• Scholarships & Certificates" << endl;
+    cout << " - academic excellence : 1,680,000(KRW)" << endl << endl;
+
+    return 0;
+}
